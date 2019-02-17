387aac1c31a47387b2754f218b96c30861a28ed2
TechLee <techlee@qq.com>
2019-02-17 23:55:06 +0800

    update database config

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
     // 密码
     'password' => Env::get('database.password'),
     // 端口
-    'hostport' => '',
+    'hostport' => Env::get('database.hostport', 3306),
     // 连接dsn
     'dsn' => '',
     // 数据库连接参数
--- public/index.php
+++ public/index.php
@@ -17,7 +17,10 @@ namespace think;
 // 加载基础文件
 require __DIR__.'/../thinkphp/base.php';
 
-require __DIR__.'/../../tech-admin/vendor/autoload.php';
+if (file_exists(__DIR__.'/../../tech-admin/vendor/autoload.php')) {
+    require __DIR__.'/../../tech-admin/vendor/autoload.php';
+}
+
 // 支持事先使用静态方法设置Request对象和Config对象
 
 // 执行应用并响应
