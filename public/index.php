6b29971795ad18a0283d516b029fc8fddd87c695
TechLee <techlee@qq.com>
2019-02-24 22:28:25 +0800

    增加官网，修改权限策略

--- public/index.php
+++ public/index.php
@@ -17,10 +17,6 @@ namespace think;
 // 加载基础文件
 require __DIR__.'/../thinkphp/base.php';
 
-if (file_exists(__DIR__.'/../../tech-admin/vendor/autoload.php')) {
-    require __DIR__.'/../../tech-admin/vendor/autoload.php';
-}
-
 // 支持事先使用静态方法设置Request对象和Config对象
 
 // 执行应用并响应
